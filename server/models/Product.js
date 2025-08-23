a08d0011d92871e0745e975d67026e1efba37cc4
Jason Arnold <jaarnoljhb024@student.wethinkcode.co.za>
2025-08-23 12:51:14 +0200

    Add Sequelize models for Product, Sale, and User with schema definitions

---
+++ server/models/Product.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Product = sequelize.define('Product', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  barcode: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
+
+module.exports = Product;
---
+++ server/models/Sale.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Sale = sequelize.define('Sale', {
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  total: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = Sale;
+
